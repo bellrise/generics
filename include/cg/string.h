edb512b41db43d0b855e640543466e5d1a328f39
bellrise <bellrise.dev@gmail.com>
2021-11-15 13:03:15 +0100

    string: assign() & assignment operator
    Assigning another string to this string copies it over.

--- include/cg/string.h
+++ include/cg/string.h
@@ -23,9 +23,12 @@ class String
 {
 public:
     String();
+
+    /* These constructors will copy the string and put it in this string. */
     String(char const *str);
     String(String const &str);
 
+    /* Free all used resources. */
     ~String();
 
     /* Return the pointer to the string for libc functions. */
@@ -37,15 +40,30 @@ public:
     /* Returns true if both strings are equal. */
     bool equals(String const &other);
 
+    /* Returns a new copy of the string. */
+    String copy() const;
+
     /* Append the other string to this string. */
     void append(String const &other);
     void append(char const *other);
     void append(char const *other, size_t len);
 
-    /* Conveniece operator overloads. */
+    /* Assigning a different string to this string will remove the original one,
+       and copy the other one it its place. */
+    void assign(String const &other);
+    void assign(char const *other);
+
+    /* Comparison operator, calls equals(). */
     bool operator==(String const &other);
+
+    /* Addition operator, returns a copy of the created string. */
     String operator+(String const &other);
+
+    /* Append operator, appends other string to the existing string. */
     void operator+=(String const &other);
+
+    /* Assignment operator. */
+    void operator=(String const &other);
 
 private:
     size_t  m_size;
@@ -55,7 +73,10 @@ private:
     /* Allocate `bytes` bytes round up to the nearest granularity defined by
        CG_STRING_ALLOC_G. This may cut the string if the requested size is
        smaller than the existing string. */
-    void alloc(size_t bytes);
+    void _alloc(size_t bytes);
+
+    /* Clear this string. Used by both the deconstructor and assign(). */
+    void _free();
 };
 
 _CG_END
--- src/string.cc
+++ src/string.cc
@@ -19,7 +19,7 @@ String::String(char const *str) : m_val(NULL)
     if (!size)
         return;
 
-    alloc(size);
+    _alloc(size);
     m_len = size;
 
     memcpy(m_val, str, m_len);
@@ -28,7 +28,7 @@ String::String(char const *str) : m_val(NULL)
 
 String::String(String const &str)
 {
-    /* The copy constructor also needs to copy the m_val allocation. */
+    // The copy constructor also needs to copy the m_val allocation.
     m_len = str.m_len;
     m_size = str.m_size;
 
@@ -39,7 +39,7 @@ String::String(String const &str)
 
 String::~String()
 {
-    free(m_val);
+    _free();
 }
 
 size_t String::len() const
@@ -49,36 +49,30 @@ size_t String::len() const
 
 char const* String::get() const
 {
-    /* If the length is 0, we still need to ensure that we pass a null
-       terminated string to libc functions, so this hack returns the address
-       of the m_len variable which will start with a null byte, imitating
-       an empty string. */
+    // If the length is 0, we still need to ensure that we pass a null
+    // terminated string to libc functions, so this hack returns the address
+    // of the m_len variable which will start with a null byte, imitating
+    // an empty string.
     if (!m_len)
         return (char *) &m_len;
 
     return m_val;
 }
 
-void String::alloc(size_t bytes)
-{
-    size_t alloc_size;
-
-    alloc_size = bytes - (bytes % CG_STRING_ALLOC_G);
-    alloc_size += CG_STRING_ALLOC_G;
-
-    printf("alloc %zu (%zu)\n", bytes, alloc_size);
-
-    m_size = alloc_size;
-    m_val = (char *) realloc(m_val, m_size);
-    m_val[m_size-1] = 0;
-}
-
 bool String::equals(String const &other)
 {
+    if (this == &other)
+        return true;
+
     if (m_len != other.m_len)
         return false;
 
     return strncmp(m_val, other.m_val, m_size) == 0;
+}
+
+String String::copy() const
+{
+    return String(*this);
 }
 
 void String::append(String const &other)
@@ -93,14 +87,14 @@ void String::append(char const *other)
 
 void String::append(char const *other, size_t len)
 {
+    // Append a C-style string to this string. If the string cannot fit, it
+    // will allocate another CG_STRING_ALLOC_G * n bytes to fit the string.
     if (len > m_size - m_len - 1)
-        alloc(m_len + len);
+        _alloc(m_len + len);
 
     strncpy(m_val + m_len, other, len);
     m_len += len;
 }
-
-/* Convenience operator overloads. */
 
 bool String::operator==(String const &other)
 {
@@ -109,6 +103,8 @@ bool String::operator==(String const &other)
 
 String String::operator+(String const &other)
 {
+    // Because this may be used outside of the assignment operator, it needs
+    // to return a new copy of the string.
     String new_str(m_val);
     new_str.append(other);
     return new_str;
@@ -117,6 +113,51 @@ String String::operator+(String const &other)
 void String::operator+=(const String &other)
 {
     append(other.m_val);
+}
+
+void String::assign(String const &other)
+{
+    _free();
+
+    String str(other);
+
+    // Steal the string
+    m_size = str.m_size;
+    m_len  = str.m_len;
+    m_val  = str.m_val;
+    str.m_val  = NULL;
+    str.m_len  = 0;
+    str.m_size = 0;
+}
+
+void String::assign(char const *other)
+{
+    assign(String(other));
+}
+
+void String::operator=(String const &other)
+{
+    assign(other);
+}
+
+void String::_alloc(size_t bytes)
+{
+    size_t alloc_size;
+
+    alloc_size = bytes - (bytes % CG_STRING_ALLOC_G);
+    alloc_size += CG_STRING_ALLOC_G;
+
+    m_size = alloc_size;
+    m_val = (char *) realloc(m_val, m_size);
+    m_val[m_size-1] = 0;
+}
+
+void String::_free()
+{
+    m_len  = 0;
+    m_size = 0;
+    free(m_val);
+    m_val  = NULL;
 }
 
 _CG_END
